982d40d9cfaf72541ada027dd747397265f59f7d
Smoooky42 <Fazekash42@yandex.ru>
2023-03-06 00:27:16 +0300

    Add Midlleware

--- server/error/ApiError.js
+++ server/error/ApiError.js
@@ -11,9 +11,11 @@ class ApiError extends Error{
 
     static internal(message) {
         return new ApiError(500, message)
-    }
+    } 
 
     static forbidden(message) {
         return new ApiError(403, message)
     }
 }
+
+module.exports = ApiError
