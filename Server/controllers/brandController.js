f5b66935d883e629cfa9ec7bf8c34223e200463c
Smoooky42 <Fazekash42@yandex.ru>
2023-06-29 17:25:01 +0300

    Added routes and controllers

---
+++ Server/controllers/brandController.js
@@ -0,0 +1,11 @@
+class BrandController{
+    async creat(req, res) {
+
+    }
+
+    async getAll(req, res) {
+
+    }
+}
+
+module.exports = new BrandController()
---
+++ Server/controllers/typeController.js
@@ -0,0 +1,11 @@
+class TypeController{
+    async creat(req, res) {
+
+    }
+
+    async getall(req, res) {
+
+    }
+}
+
+module.exports = new TypeController()
