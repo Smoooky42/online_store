174f29b83c590d370b30f8eacdf818878d5fe496
Smoooky42 <Fazekash42@yandex.ru>
2023-07-04 20:21:48 +0300

    Added static

--- Server/index.js
+++ Server/index.js
@@ -6,12 +6,13 @@ const cors = require('cors')
 const fileupload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
-
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileupload({}))
 app.use('/api', router)
 
--- Server/routes/deviceRouter.js
+++ Server/routes/deviceRouter.js
@@ -7,4 +7,4 @@ router.get('/', deviceController.getAll)
 router.get('/:id', deviceController.getOne)
 
 
-module.exports = router 
+module.exports = router
